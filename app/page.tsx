2e8c2fe2a1cf9a2c29072f72f6ac3b797c69763e
Niraj Kshirsagar <nirajkshirsagar72@gmail.com>
2026-02-06 00:38:37 +0530

    update(folder): moved components out of app folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Navigation from "./components/Navigation";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import ProductPreview from "./components/ProductPreview";
-import SocialProof from "./components/SocialProof";
-import UseCases from "./components/UseCases";
-import Pricing from "./components/Pricing";
-import CTA from "./components/CTA";
-import Footer from "./components/Footer";
+import Navigation from "../components/Navigation";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import ProductPreview from "../components/ProductPreview";
+import SocialProof from "../components/SocialProof";
+import UseCases from "../components/UseCases";
+import Pricing from "../components/Pricing";
+import CTA from "../components/CTA";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
